b8eeed491aadea811f07e6d57956483d944c6a52
shahboz <shahbozmamatkarimov2303@gmail.com>
2023-03-05 12:51:45 +0300

    partfolio

---
+++ index.js
@@ -0,0 +1,48 @@
+// our works
+let background = [
+    "https://www.siko.cz/medias/gn-mramor-koupelna-012.jpg?context=bWFzdGVyfGF6dXJlaW1hZ2VzfDIxOTk3MXxpbWFnZS9qcGVnfGgyYS9oN2IvOTY4MDMyNTQ3NjM4Mi9nbi1tcmFtb3Ita291cGVsbmFfMDEyLmpwZ3w2NDk5MTRmOTIwZDU2YzQxN2ZlZjFhNTA0ZDNhMjg3NDJjODg5MzU1MmM4NzNkZGY5NDNkYjY4NzFjNWVhYjEy",
+    "https://1303378420.rsc.cdn77.org/getfile.aspx?id_file=686797766",
+    "https://colodu.club/uploads/posts/2022-11/1667385676_13-colodu-club-p-fasad-zhidkii-travertin-krasivo-14.jpg",
+    "https://colodu.club/uploads/posts/2022-11/1667385734_39-colodu-club-p-fasad-zhidkii-travertin-krasivo-43.jpg",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/jit37728si453-UZ/image;s=710x703",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/e6fdoay5xr8q-UZ/image;s=1280x960",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/kbh7v4o7sqga-UZ/image;s=718x718",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/y48utchi2snl1-UZ/image;s=1080x807",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/4an7vc84p9eu2-UZ/image",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/bcpzuv1rk6jo1-UZ/image",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/p6qte1qjg6pc3-UZ/image;s=1280x1280",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/9fjt1wkse7nj-UZ/image",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/jrggm5j9lfuf1-UZ/image;s=576x1280",
+    "https://frankfurt.apollo.olxcdn.com/v1/files/jrggm5j9lfuf1-UZ/image;s=576x1280",
+    "https://bazzar.uz/wp-content/uploads/2022/06/image-2022-06-12T093113.206.jpg"
+]
+let back = 1;
+setInterval(() => {
+    ourworks.style = "width: 100%; height: 600px !important;"
+    ourworks.src = `${background[back]}`
+    back += 1;
+    if (back == background.length) {
+        back = 0;
+    }
+}, 2000)
+
+
+// image slider
+let slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    if (n > background.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = background.length }
+    for (i = 0; i < background.length; i++) {
+        slider.src = background[slideIndex-1]
+    }
+    number.innerHTML = slideIndex + ' / 3'
+}
+
+function types(val){
+    localStorage.setItem("type", val.innerHTML)
+}
---
+++ js/types.js
@@ -0,0 +1,88 @@
+const stoneType = localStorage.getItem('type');
+
+const mramor = [
+    "https://www.drevoobchod-eshop.cz/editor/image/eshop_products_other_pictures/58727/file_en_58727_l.jpg",
+    "https://www.kammel.sk/resize/e/1200/1200/files/mramor-teplakovina-pes2.jpg",
+    "https://www.interierstone.cz/cardfiles/card-19834/card-19959/img/c67e674b89393e33f008b41ece58d239.jpg",
+    "https://www.perfecto.cz/picture/i%2Fs%2Fis%20mramor%20stribrno-sedy_def_I3wjfDUwMHwjfCN8I3wjfCN8MXwjfCN8MQ%3D%3D.jpg&v=726",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRujCp_RjgDNA8TFkCHNbqHdIlKb_E-VVMg3A&usqp=CAU",
+    "https://images.prom.ua/3102213396_w640_h640_gibkij-kamen-grand.jpg",
+    "https://mramor-comerce.hr/wp-content/uploads/2020/11/ROSA-PORINNO.jpg",
+    "https://milujemekameny.cz/wp-content/uploads/2019/05/mramor_milujemekameny_cz.png",
+    "https://cdn.pixabay.com/photo/2016/12/02/17/34/texture-1878242__480.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRitX8pmckLk6VfVvR-YUdMrK4MbQpLo5YZ1mVXEWyj6tJaG-YvdRSlTZg2J0Q3VR-oLns&usqp=CAU"
+]
+
+const marmar = [
+    "https://i1.wp.com/kamen.expert/wp-content/uploads/2017/12/belyj_kamen.jpg",
+    "https://i0.wp.com/kamen.expert/wp-content/uploads/2017/12/chernyj_kamen.jpg",
+    "https://i2.wp.com/kamen.expert/wp-content/uploads/2017/12/zelenyj_kamen.jpg",
+    "https://i0.wp.com/kamen.expert/wp-content/uploads/2017/12/krasnyj_kamen-1.jpg",
+    "https://i1.wp.com/kamen.expert/wp-content/uploads/2017/12/seryj_kamen.jpg",
+    "https://i1.wp.com/kamen.expert/wp-content/uploads/2017/12/bezhevyj_kamen.jpg",
+    "https://i0.wp.com/kamen.expert/wp-content/uploads/2017/12/rozovyj_kamen.jpg",
+    "https://i0.wp.com/kamen.expert/wp-content/uploads/2017/12/goluboj_kamen.jpg",
+    "https://i2.wp.com/kamen.expert/wp-content/uploads/2017/12/zheltyj_kamen.jpg",
+    "http://cdn.shopify.com/s/files/1/0069/1313/0561/products/Mramor-obrazova-tapeta-2020-foto_1024x1024.jpg?v=1665575874"
+]
+
+const granit = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Wv2x14scDLurVq1STueR0aDnI4T4wiDrozfY5vsmgU_RFOhdi7D5u57XzSczXvwh3j4&usqp=CAU",
+    "https://imageio.forbes.com/blogs-images/trevornace/files/2016/06/blue-granite-1200x957.jpg?format=jpg&width=960",
+    "https://imageio.forbes.com/blogs-images/trevornace/files/2016/06/black-white-granite-countertops-1200x833.jpg?height=493&width=711&fit=bounds",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYQm4XhBnoJGRsCeFGiR-GCj77Qa1k_HM-XQ&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW_jDeTIHzhfORyMbckn6n-8q5HhQJZY29nw&usqp=CAU",
+    "https://cdn-ablmn.nitrocdn.com/oMKSNsZXrYMXmtrNmIEOjiWcSeXWPnAX/assets/images/optimized/rev-1f76c59/wp-content/uploads/Black-Angola-Granite--600x600.jpeg",
+    "https://uploads-ssl.webflow.com/6259fa3a193e9d1afdf459b2/62f052e52b7f3e0102b35f59_msi-surfaces-surprise-granite-arctic-sand-granite-close%20up.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa9np5LpX_bAyT3XYZYwGnbJq4SFw_l5KmrHAstdePsz0OpShZh_Zu2kmlrpzlQUVc-6Y&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ5ylJwpls3ct9WSGb1KHCdRPf17VnWGYG-MMjFc9ZVjJgpSe2LfQXuNXIwemMwbxjsTA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_cnn5xzz2yC_0bIsCAfzKpv-c46KyVDI5KA&usqp=CAU",
+]
+
+const travertin = [
+    "https://www.dedalostone.com/wp-content/uploads/2021/07/travertine-800x400.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIC5p3z2ARrfsssl2wvzh0gLAw1-BYqQHJkJ6UlMbvBHIRhaQKGq7P9U2DIsQNPue4ihA&usqp=CAU",
+    "https://cdn.shopify.com/s/files/1/0261/1567/0094/files/silverTravertinePolishedLarge.jpg?v=1566302032",
+    "https://www.dedalostone.com/wp-content/uploads/2021/07/gray-travertine-800x800.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7BIEtfnOtVqHCbOrCpz0OyS1olH809vOxSwBcyeWutwggAT_zCGDTREvPVmSkfoqO478&usqp=CAU",
+    "https://previews.123rf.com/images/pilgrimego/pilgrimego1905/pilgrimego190500050/123838586-natural-pattern-of-marble-brown-color-polished-slice-mineral-super-high-resolution-travertin.jpg",
+    "https://us.123rf.com/450wm/morele/morele2004/morele200400062/144802653-natural-stone-in-the-colors-of-lush-stripes-with-stripes-called-travertin-rosso.jpg?ver=6",
+    "https://www.mgtstoneco.com/wp/wp-content/uploads/2021/11/Grey-Travertine-Surface.jpg",
+    "https://t3.ftcdn.net/jpg/02/70/74/28/360_F_270742823_J04UOEItyjBQyJOeZWcBiykgxcqyp3Bz.jpg",
+    "https://www.sydneytilegallery.com.au/wp-content/uploads/2019/02/outdoor-travertine-tile.jpg"
+]
+
+console.log(stoneType);
+console.log("Hello");
+const stones = [mramor, marmar, granit, travertin]
+const stones1 = ["Mramor", "Marmar", "Granit", "Travertin"]
+
+let type = 0;
+for (let i in stones1) {
+    if (stoneType == stones1[i]) {
+        type = i;
+    }
+}
+
+for (let img of stones[type]) {
+    tbody.innerHTML += `
+        <tr>
+            <td>
+                <div class="col-12">
+                    <img src=${img} class="img-thumbnail">
+                </div>
+            </td>
+        </tr>
+        `
+}
+
+function types(val) {
+    localStorage.setItem("type", val.innerHTML)
+}
+
+const title = document.querySelectorAll(".title");
+console.log(title);
+for (let i of title){
+    console.log(i);
+    i.innerHTML = stoneType
+}
